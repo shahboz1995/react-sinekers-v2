6ef739b90cc9e6c9e6634d40f0f23b2f9d76b375
Shaxboz <shaxbozali9@gmail.com>
2023-05-16 12:11:09 +0300

    set up redux thunk and json server

--- src/redux/sneakersReducer.js
+++ src/redux/sneakersReducer.js
@@ -5,17 +5,25 @@ const initialState = {
 };
 
 const addTofavourite = (state, item) => {
-  console.log(item);
   return {
     ...state,
     favorites: [...state.favorites, item],
   };
 };
+const saveAllSneakers =(state, allSneakers = [])=>{
+  return {
+    ...state,
+    allSneakers: allSneakers,
+  }
+}
 
 const sneakersReducer = (state = initialState, action) => {
   switch (action.type) {
     case 'ADD_TO_FAVOURITE':
       return addTofavourite(state, action.payload);
+      case `SAVE_ALL_SNEAKERS`:
+      return saveAllSneakers(state, action.payload);
+
     default:
       return state;
   }
